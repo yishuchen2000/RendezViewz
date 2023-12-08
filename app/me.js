2584a599e7e6688c9256a98c98255f104488d2e0
Allen Naliath <allennaliath@gmail.com>
2023-12-08 10:56:29 -0800

    profile page done

--- app/me.js
+++ app/me.js
@@ -6,14 +6,19 @@ import {
   ScrollView,
   SafeAreaView,
   Image,
+  FlatList,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { FontAwesome } from "@expo/vector-icons";
+import ProfilePost from "../components/ProfilePost";
 
-export default function App() {
+export default function Me() {
   return (
     <SafeAreaView style={styles.container}>
-      <LinearGradient colors={["#361866", "#E29292"]} style={styles.container}>
+      <LinearGradient
+        colors={["#361866", "#E29292"]}
+        style={[styles.container, { paddingHorizontal: 8 }]}
+      >
         <View style={styles.titleBar}>
           <FontAwesome name="gear" size={24} color="white" />
         </View>
@@ -58,28 +63,32 @@ export default function App() {
           </View>
         </View>
 
-        <View style={{ marginTop: 24 }}>
-          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-            <View style={styles.mediaImageContainer}>
-              <Image
-                source={require("../assets/Alexa.png")}
-                style={styles.image}
-                resizeMode="cover"
-              ></Image>
-            </View>
-            <View style={styles.mediaImageContainer}>
-              <Image
-                source={require("../assets/Alexa.png")}
-                style={styles.image}
-                resizeMode="cover"
-              ></Image>
-            </View>
-            <View style={styles.mediaImageContainer}>
-              <Image
-                source={require("../assets/Alexa.png")}
-                style={styles.image}
-                resizeMode="cover"
-              ></Image>
+        <Text style={[styles.subText, styles.recent]}>My posts</Text>
+        <View style={{ height: 175 }}>
+          <ScrollView horizontal={false}>
+            <View>
+              <ProfilePost
+                id={1}
+                user={"Yishu C."}
+                timestamp={"now"}
+                text={"Anything with Margot Robbie hitsss"}
+                liked={false}
+                imageSource={require("../assets/barbieposter.jpg")}
+                profilePic={require("../assets/YishuDog.jpg")}
+                action={'Ranked "Barbie" #2'}
+                // comments={item.comments}
+              />
+              <ProfilePost
+                id={1}
+                user={"Yishu C."}
+                timestamp={"2h "}
+                text={"I can't believe Rachel"}
+                liked={false}
+                imageSource={require("../assets/friendsposter.jpeg")}
+                profilePic={require("../assets/YishuDog.jpg")}
+                action={'Now on episode 6 of "Friends"'}
+                // comments={item.comments}
+              />
             </View>
           </ScrollView>
         </View>
@@ -92,9 +101,9 @@ export default function App() {
               <Text
                 style={[styles.text, { color: "white", fontWeight: "300" }]}
               >
-                Started following{" "}
-                <Text style={{ fontWeight: "400" }}>Yishu C.</Text> and{" "}
-                <Text style={{ fontWeight: "400" }}>Francis S.</Text>
+                Now friends with <Text style={styles.boldName}>Allen N.</Text>,{" "}
+                <Text style={styles.boldName}>Francis S.</Text>,{" "}
+                <Text style={styles.boldName}>+1 more</Text>
               </Text>
             </View>
           </View>
@@ -105,9 +114,8 @@ export default function App() {
               <Text
                 style={[styles.text, { color: "white", fontWeight: "300" }]}
               >
-                Started following{" "}
-                <Text style={{ fontWeight: "400" }}>Yishu C.</Text> and{" "}
-                <Text style={{ fontWeight: "400" }}>Francis S.</Text>
+                <Text style={styles.boldName}>Zach</Text> is inviting you to
+                watch <Text style={styles.boldName}>Black Mirror</Text>
               </Text>
             </View>
           </View>
@@ -118,9 +126,20 @@ export default function App() {
               <Text
                 style={[styles.text, { color: "white", fontWeight: "300" }]}
               >
-                Started following{" "}
-                <Text style={{ fontWeight: "400" }}>Yishu C.</Text> and{" "}
-                <Text style={{ fontWeight: "400" }}>Francis S.</Text>
+                <Text style={styles.boldName}>Charlotte Z.</Text> liked your
+                post
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.recentItemCard}>
+            <View style={styles.activityIndicator}></View>
+            <View style={{ width: 300 }}>
+              <Text
+                style={[styles.text, { color: "white", fontWeight: "300" }]}
+              >
+                <Text style={styles.boldName}>Alexa</Text> is inviting you to
+                watch <Text style={styles.boldName}>Invincible</Text>
               </Text>
             </View>
           </View>
@@ -157,8 +176,8 @@ const styles = StyleSheet.create({
     marginBottom: -32,
   },
   profileImage: {
-    width: 200,
-    height: 200,
+    width: 150,
+    height: 150,
     borderRadius: 100,
     overflow: "hidden",
   },
@@ -190,8 +209,8 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
   },
   recent: {
-    marginLeft: 24,
-    marginTop: 32,
+    marginLeft: 12,
+    marginTop: 16,
     marginBottom: 6,
     fontSize: 18,
   },
@@ -203,7 +222,7 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     paddingHorizontal: 12,
     backgroundColor: "rgba(255, 255, 255, 0.3)",
-    borderRadius: 4,
+    borderRadius: 10,
   },
   activityIndicator: {
     backgroundColor: "white",
@@ -213,5 +232,8 @@ const styles = StyleSheet.create({
     borderRadius: 6,
     marginTop: 3,
     marginRight: 20,
+  },
+  boldName: {
+    fontWeight: "500",
   },
 });
