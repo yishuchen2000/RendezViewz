a129fa675971fc4604a5de91c807525b9897fb62
charlottezhu1 <127057825+charlottezhu1@users.noreply.github.com>
2024-03-02 20:48:17 -0800

    yo

--- components/Account.tsx
+++ components/Account.tsx
@@ -1,9 +1,21 @@
 import { useState, useEffect } from "react";
 import supabase from "../Supabase";
-import { StyleSheet, View, Alert, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Alert,
+  Text,
+  Dimensions,
+  TouchableHighlight,
+  SafeAreaView,
+} from "react-native";
 import { Button, Input } from "react-native-elements";
+import { LinearGradient } from "expo-linear-gradient";
 import { Session } from "@supabase/supabase-js";
 import { FlashList } from "@shopify/flash-list";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export default function Account({ session }: { session: Session }) {
   const [loading, setLoading] = useState(true);
@@ -87,66 +99,124 @@ export default function Account({ session }: { session: Session }) {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Input label="Email" value={session?.user?.email} disabled />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Input
-          label="Username"
-          value={username || ""}
-          onChangeText={(text) => setUsername(text)}
-        />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Input
-          label="Website"
-          value={website || ""}
-          onChangeText={(text) => setWebsite(text)}
-        />
-      </View>
+    <LinearGradient
+      colors={["#311866", "#b67287"]}
+      style={[styles.container, { paddingHorizontal: 40 }]}
+    >
+      <Text style={styles.title}>Edit Account</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={[styles.verticallySpaced, styles.mt20]}>
+          <Input
+            label="Username"
+            value={username || ""}
+            labelStyle={styles.textWhite}
+            onChangeText={(text) => setUsername(text)}
+            inputStyle={styles.textWhite}
+          />
+        </View>
+        <View style={styles.verticallySpaced}>
+          <Input
+            label="Email"
+            value={session?.user?.email}
+            disabled
+            labelStyle={styles.textWhite}
+            inputStyle={styles.textWhite}
+          />
+        </View>
 
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Button
-          title={loading ? "Loading ..." : "Update"}
-          onPress={() =>
-            updateProfile({ username, website, avatar_url: avatarUrl })
-          }
-          disabled={loading}
-          buttonStyle={{ backgroundColor: "#602683" }}
-        />
-      </View>
+        <View style={[styles.buttonSpaced, styles.mt20]}>
+          <TouchableHighlight
+            activeOpacity={0.6} // Adjust the opacity as needed
+            underlayColor="transparent" // Set the underlay color to transparent
+          >
+            <Button
+              title={loading ? "Loading ..." : "Update"}
+              onPress={() =>
+                updateProfile({ username, website, avatar_url: avatarUrl })
+              }
+              disabled={loading}
+              titleStyle={{ color: "white" }}
+              buttonStyle={{
+                backgroundColor: "#311866",
+                borderRadius: 20,
+                height: 50,
+                paddingHorizontal: 20,
+              }}
+            />
+          </TouchableHighlight>
+        </View>
 
-      <View style={styles.verticallySpaced}>
-        <Button
-          title="Sign Out"
-          onPress={() => supabase.auth.signOut()}
-          buttonStyle={{ backgroundColor: "#602683" }}
-        />
-      </View>
+        <View style={styles.buttonSpaced}>
+          <TouchableHighlight
+            activeOpacity={0.6} // Adjust the opacity as needed
+            underlayColor="transparent" // Set the underlay color to transparent
+          >
+            <Button
+              title="Sign Out"
+              onPress={() => supabase.auth.signOut()}
+              titleStyle={{ color: "white" }}
+              disabled={loading}
+              buttonStyle={{
+                backgroundColor: "transparent",
+                borderColor: "white",
+                borderWidth: 2,
+                borderRadius: 20,
+                height: 50,
+                paddingHorizontal: 20,
+              }}
+            />
+          </TouchableHighlight>
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
+  );
+}
 
-      {/* <View style={[styles.verticallySpaced, { height: 200 }]}>
+{
+  /* <View style={[styles.verticallySpaced, { height: 200 }]}>
         <FlashList
           data={users}
           renderItem={({ item }) => <Text>{item.id}</Text>}
           estimatedItemSize={200}
         />
-      </View> */}
-    </View>
-  );
+      </View> */
 }
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 40,
-    padding: 12,
+    width: windowWidth,
+    height: windowHeight,
+    alignItems: "center",
+    // backgroundColor: "#001D3D",
+  },
+  title: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 20,
+    marginTop: 30,
+    textAlign: "left", // Align text to the left
+    alignSelf: "flex-start",
+  },
+  textWhite: {
+    color: "white", // Set text color to white
   },
   verticallySpaced: {
+    paddingHorizontal: 40,
     paddingTop: 4,
-    paddingBottom: 4,
+    paddingBottom: 2,
+    alignSelf: "stretch",
+  },
+  buttonSpaced: {
+    paddingHorizontal: 40,
+    paddingTop: 4,
+    paddingBottom: 12,
     alignSelf: "stretch",
   },
   mt20: {
-    marginTop: 20,
+    marginTop: 25,
   },
 });
